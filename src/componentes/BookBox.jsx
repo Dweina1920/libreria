48e41bf69ac8e0321d2099f431867f99a4bb5c5a
unknown <nadialejandra87@gmail.com>
2022-06-29 10:29:12 +0200

    cambios de nombres de archivosnadia

--- src/componentes/BookBox.jsx
+++ src/componentes/BookBox.jsx
@@ -1,8 +1,8 @@
-import books from "../data/data.json";
+import books from "../data/violetbooks.json";
 import BookItem from "./BookItem";
 
 function BookBox() {
-    const listBooks = books.map((book) => {
+    const listBooks = books.violetBooks.map((book) => {
         return (<BookItem key={book.id} name={book.bookname} author={book.author} image={book.image} />)
     }
     )
