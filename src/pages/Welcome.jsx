a96ccdb7d09e9abb49e58bc9d81ec4f6f80168ca
Dweina1920 <dina.yamanid@gmail.com>
2022-07-06 10:04:26 +0200

    welcome

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import Header from "../componentes/Header";
+import "../index.css";
+import React from "react";
+
+
+function Welcome(){
+    return(
+        <div className="welcome">
+            <Header/>
+            <div className="div-welcome">
+                    <p className="title-welcome">Bienvenidos a Violet Librery</p>
+                    <Link className="button-welcome" to="/Home">➨ Adelante</Link>
+            </div>
+        </div>
+    )
+
+}
+
+export default Welcome;
